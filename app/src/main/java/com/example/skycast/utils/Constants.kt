afd1c7c9f0211bcb6e0f2aa30cb106a3b6d320d5
Youssef Abd El-Fatah <youssefabdelfatah258@gmail.com>
2026-03-10 03:59:45 +0200

    fix a bug

--- app/src/main/java/com/example/skycast/utils/Constants.kt
+++ app/src/main/java/com/example/skycast/utils/Constants.kt
@@ -3,7 +3,7 @@ package com.example.skycast.utils
 import com.example.skycast.BuildConfig
 
 object Constants {
-    const val API_KEY = BuildConfig.WEATHER_API_KEY
+    const val API_KEY = BuildConfig.MAPS_API_KEY
     const val BASE_URL = "https://api.openweathermap.org/"
 
     // We can also add default shared preferences keys here later
